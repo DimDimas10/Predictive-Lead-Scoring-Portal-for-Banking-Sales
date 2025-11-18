a2d59a1d821ac6eedbff84c9410ac3e565b22407
SaurSRT <107399439+SaurSRT@users.noreply.github.com>
2025-11-18 13:34:09 +0700

    handler.js

--- CAPSTONE-BACKEND/src/api/leads/handler.js
+++ CAPSTONE-BACKEND/src/api/leads/handler.js
@@ -1,7 +1,6 @@
-let leads = require('../../data/leads'); // Gunakan let karena data akan dimutasi
+let leads = require('../../data/leads');
 
 const getLeadsHandler = (request, h) => {
-  // Simulasi loading 1.5 detik
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(h.response(leads).code(200));
@@ -14,7 +13,6 @@ const getLeadByIdHandler = (request, h) => {
   const lead = leads.find((l) => l.id === id);
 
   if (lead) {
-    // Simulasi loading 0.5 detik
     return new Promise(resolve => {
       setTimeout(() => {
         resolve(h.response(lead).code(200));
@@ -66,4 +64,5 @@ module.exports = {
   getLeadByIdHandler,
   updateLeadStatusHandler,
   updateLeadNotesHandler,
+
 };
