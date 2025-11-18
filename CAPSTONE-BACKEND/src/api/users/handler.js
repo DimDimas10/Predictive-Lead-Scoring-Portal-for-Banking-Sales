bc3c5ae12cce746261033325bba917f9fee586e0
SaurSRT <107399439+SaurSRT@users.noreply.github.com>
2025-11-18 13:34:49 +0700

    handler.js

--- CAPSTONE-BACKEND/src/api/users/handler.js
+++ CAPSTONE-BACKEND/src/api/users/handler.js
@@ -6,12 +6,12 @@ const loginHandler = (request, h) => {
   const user = users.find((u) => u.email === email && u.password === password);
 
   if (user) {
-    // Jangan kirim password ke frontend
     const { password, ...userData } = user;
     return h.response(userData).code(200);
   }
 
   return h.response({ message: 'Email atau password salah.' }).code(401);
 };
+
 
 module.exports = { loginHandler };
