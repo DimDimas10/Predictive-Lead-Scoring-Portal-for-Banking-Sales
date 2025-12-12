69ba3a67ae978c88cf0e273a860e055df9316683
Dimas Aditya <136893064+DimDimas10@users.noreply.github.com>
2025-12-12 19:03:42 +0700

    Update db.js

--- CAPSTONE-BACKEND/src/db.js
+++ CAPSTONE-BACKEND/src/db.js
@@ -1,17 +1,12 @@
 // src/db.js
-require('dotenv').config(); // Tambahkan ini biar aman saat dev local
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'yamabiko.proxy.rlwy.net',
-  port: process.env.DB_PORT || 55668,
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'BJzuwxrRAfUittZkDZzWLYABOisGhRUq',
-  database: process.env.DB_NAME || 'railway',
-  // TAMBAHKAN BAGIAN INI:
-  ssl: {
-    rejectUnauthorized: false
-  }
+  password: process.env.DB_PASSWORD || 'dimasgundar10',
+  database: process.env.DB_NAME || 'predictive_bank',
 });
 
 module.exports = pool;
