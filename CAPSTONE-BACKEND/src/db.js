74df0970d233de58e411068d3f168fce74f81d24
Dimas Aditya <136893064+DimDimas10@users.noreply.github.com>
2025-12-12 15:08:05 +0700

    Update db.js

--- CAPSTONE-BACKEND/src/db.js
+++ CAPSTONE-BACKEND/src/db.js
@@ -1,4 +1,5 @@
 // src/db.js
+require('dotenv').config(); // Tambahkan ini biar aman saat dev local
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,7 +8,10 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'BJzuwxrRAfUittZkDZzWLYABOisGhRUq',
   database: process.env.DB_NAME || 'railway',
+  // TAMBAHKAN BAGIAN INI:
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
-
