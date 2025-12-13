79ee3be6eeb71a1697ef575fb58df1fd4ed5f34f
Dimas Aditya <136893064+DimDimas10@users.noreply.github.com>
2025-12-13 17:37:06 +0700

    Update AdminUserPage.tsx

--- CAPSTONE/src/components/AdminUserPage.tsx
+++ CAPSTONE/src/components/AdminUserPage.tsx
@@ -10,7 +10,7 @@ import { ModalPortal } from './ModalPortal';
 import Swal from 'sweetalert2';
 import { useThemeLanguage } from '../contexts/ThemeLanguageContext';
 
-const API_URL = 'http://localhost:5000/api';
+import { API_URL } from '../config/api';
 
 interface AdminUserPageProps {
   user: User;
@@ -323,4 +323,5 @@ export function AdminUserPage({ user, onLogout, onBackToDashboard }: AdminUserPa
       )}
     </div>
   );
+
 }
